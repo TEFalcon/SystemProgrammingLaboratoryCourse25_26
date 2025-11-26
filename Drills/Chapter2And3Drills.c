3c286f1cb27ae4281a41dbee29c9b69f0a234652
Eyal Kalish <99074066+TEFalcon@users.noreply.github.com>
2025-11-26 16:40:45 +0200

    done drills 1, started chapters 2 and 3

---
+++ Drills/Chapter2And3Drills.c
@@ -0,0 +1,15 @@
+/**
+ * @file Chapter2And3Drills.c
+ * @author Eyal K.
+ * @date Nov25th 2025
+ * 
+ * @brief Drills from lecture 2 of reccorded lecturer Eran from 2021B and from Chapter 2 and Chapter 3 of "The C Programming Language"
+ * 
+ * @warning these were run on the ubuntu system but writen in VScode - they were copied from start of drill to the next individually
+ * do not try to run the entire doc as is!
+ */
+
+/**
+ * @name Exercise 2-8
+ * @brief 
+  */
